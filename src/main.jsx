c91c6a0752d476b062b7d870ba2f582ce0b0cdf8
julia.andrade-at-538590928221 <qoTQt0Cryp6/ysKmwHlONj6KyLQNdpMjVwl0qeqlNns=>
2023-08-30 20:53:16 -0300

    lista de produtos + route produtos

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+//BLOCO DE ROTAS
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import App from './App.jsx'
+import EditarProdutos from './routes/EditarProdutos.jsx';
+import Erro404 from './routes/Erro404.jsx';
+import Home from './routes/Home.jsx';
+import Produtos from './routes/Produtos.jsx';
+
 import './index.css'
+
+//Criando o objeto de rotas
+const router = createBrowserRouter([
+  { path: '/', element: <App />,
+      errorElement: <Erro404 />, 
+      children: [
+        { path: '/', element: <Home /> },
+        { path: '/produtos', element: <Produtos /> },
+        { path: '/editar/produtos/:id', element: <EditarProdutos /> },
+      ]
+  },
+])
+
+//BLOCO DE ROTAS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
