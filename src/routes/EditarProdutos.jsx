6e948522f666b6b4079ecf7328a44e8b6d955f60
jujuad <juandradedias1@gmail.com>
2023-09-01 20:59:44 -0300

    aula 01/09 - icons +listagem dinamica + recuperacao de elemnetos via HOOKs useParams()

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,7 +1,27 @@
+import { useParams } from "react-router-dom";
+import { ListaProdutos } from "../Components/ListaProdutos";
 
 export default function EditarProdutos() {
-    return (
-      <div>EditarProdutos</div>
-    )
-  }
+
+  document.title = "Editar Produtos";
+
+  //O HOOK useParams()
+  const {id} = useParams();
+
+  const produtoRecuperadoPorId = ListaProdutos.filter((produto)=>{
+    if(produto.id == parseInt(id)){
+            return produto;
+        }
+      });
   
+  return (
+    <>
+      <h1>EditarProdutos</h1>
+      <p>Produto selecionado - {id}</p>
+      <p>Produto selecionado - {produtoRecuperadoPorId[0].nome}</p>
+      
+    </>
+
+
+  )
+}
